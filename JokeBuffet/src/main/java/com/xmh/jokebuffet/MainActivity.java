9532ed44a8a8b3358a5f004e23c776421eff7fb5
voidXMH <xmh19936688@163.com>
2016-06-06 08:15:13 +0000

    一个万能类Activity调通接口并将结果显示到界面

--- JokeBuffet/src/main/java/com/xmh/jokebuffet/MainActivity.java
+++ JokeBuffet/src/main/java/com/xmh/jokebuffet/MainActivity.java
@@ -1,13 +1,72 @@
 package com.xmh.jokebuffet;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
+
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.net.HttpURLConnection;
+import java.net.URL;
 
 public class MainActivity extends AppCompatActivity {
+
+    String httpUrl = "http://apis.baidu.com/showapi_open_bus/showapi_joke/joke_text";
+    String httpArg = "page=1";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                final String jsonResult = request(httpUrl, httpArg);
+                runOnUiThread(new Runnable() {
+                    @Override
+                    public void run() {
+                        ((TextView)findViewById(R.id.tv_result)).setText(jsonResult);
+                    }
+                });
+            }
+        }).start();
+    }
+
+    /**
+     * @param httpUrl
+     *            :请求接口
+     * @param httpArg
+     *            :参数
+     * @return 返回结果
+     */
+    public static String request(String httpUrl, String httpArg) {
+        BufferedReader reader = null;
+        String result = null;
+        StringBuffer sbf = new StringBuffer();
+        httpUrl = httpUrl + "?" + httpArg;
+
+        try {
+            URL url = new URL(httpUrl);
+            HttpURLConnection connection = (HttpURLConnection) url
+                    .openConnection();
+            connection.setRequestMethod("GET");
+            // 填入apikey到HTTP header
+            connection.setRequestProperty("apikey",  "033f3a4c145c2fada9a74aea0220fef0");
+            connection.connect();
+            InputStream is = connection.getInputStream();
+            reader = new BufferedReader(new InputStreamReader(is, "UTF-8"));
+            String strRead = null;
+            while ((strRead = reader.readLine()) != null) {
+                sbf.append(strRead);
+                sbf.append("\r\n");
+            }
+            reader.close();
+            result = sbf.toString();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return result;
     }
 }
